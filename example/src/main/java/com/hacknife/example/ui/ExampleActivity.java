e5c6e8910c12e33cabdaf502e7a0c58d9a5f6a1e
Hacknife <4884280@qq.com>
2018-11-23 23:31:27 +0800

    demo

--- example/src/main/java/com/hacknife/example/ui/ExampleActivity.java
+++ example/src/main/java/com/hacknife/example/ui/ExampleActivity.java
@@ -2,11 +2,16 @@ package com.hacknife.example.ui;
 
 
 import com.hacknife.example.R;
+import com.hacknife.example.adapter.VideoAdapter;
 import com.hacknife.example.bean.VideoSource;
+import com.hacknife.example.engine.IjkEngine;
 import com.hacknife.example.ui.base.BaseActivity;
 import com.hacknife.example.ui.injector.modules.ExampleModule;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 
 import com.hacknife.example.ui.view.IExampleView;
@@ -14,8 +19,15 @@ import com.hacknife.example.ui.viewmodel.ExampleViewModel;
 import com.hacknife.briefness.BindLayout;
 import com.hacknife.example.ui.injector.components.DaggerExampleActivityComponent;
 import com.hacknife.example.ui.viewmodel.i.IExampleViewModel;
+import com.hacknife.iplayer.MediaManager;
+import com.hacknife.iplayer.Player;
+import com.hacknife.iplayer.PlayerManager;
 
 import java.util.List;
+
+import javax.inject.Inject;
+
+import static com.hacknife.iplayer.state.ContainerMode.CONTAINER_MODE_FULLSCREEN;
 
 /**
  * author  : hacknife
@@ -25,6 +37,8 @@ import java.util.List;
  */
 @BindLayout(R.layout.activity_example)
 public class ExampleActivity extends BaseActivity<IExampleViewModel, ExampleActivityBriefnessor> implements IExampleView {
+    @Inject
+    VideoAdapter adapter;
 
     @Override
     protected void injector() {
@@ -35,11 +49,30 @@ public class ExampleActivity extends BaseActivity<IExampleViewModel, ExampleActi
 
     @Override
     protected void initView() {
+        Player.setPlayerEngine(new IjkEngine());
+        briefnessor.rc_view.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        briefnessor.rc_view.setAdapter(adapter);
+        briefnessor.rc_view.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+            @Override
+            public void onChildViewAttachedToWindow(View view) {
 
+            }
+
+            @Override
+            public void onChildViewDetachedFromWindow(View view) {
+                Player video = view.findViewById(R.id.iplayer);
+                if (video != null && video.getDataSource().containsTheUrl(MediaManager.getCurrentUrl())) {
+                    Player player = PlayerManager.getCurrentVideo();
+                    if (player != null && player.getContainerMode() != CONTAINER_MODE_FULLSCREEN) {
+                        Player.releaseAllPlayer();
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public void callbackVideo(List<VideoSource> dataSources) {
-
+        adapter.bindData(dataSources);
     }
 }
